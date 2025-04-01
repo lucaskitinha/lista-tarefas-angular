2bd16449c182e01919e081f03504f6006827d08a
Lucas Mesquita <lucas.lmso@gmail.com>
2025-03-31 21:30:12 -0300

    feat: lista de tarefas component

---
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+
+  public addItem = signal(true);
+
+}
